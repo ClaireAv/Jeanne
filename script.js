e4d5c99a62399391b43d28f409226455bc1592fe
Claire <avenel.cl@gmail.com>
2021-07-02 00:00:46 +0200

    branch avancée

--- script.js
+++ script.js
@@ -2,13 +2,32 @@ function showResponsiveMenu() {
     var menu = document.getElementById("topnav_menu");
     var icon = document.getElementById("topnav_icon");
     var root = document.getElementById("root");
+    // var burger = document.getElementById("burger");
+
     if (menu.className === "") {
       menu.className = "open";
       icon.className = "open";
+      // burger.className = 'active';
     } else {
       menu.className = "";                    
       icon.className = "";
+      // burger.className = '';
+
     }
   }
+
+
+// const burger = document.querySelector('.burger');
+// const ul = document.querySelector('ul');
+
+// burger.addEventListener ('mouseover', ()=>{
+//     burger.classList.toggle('active');
+// });
+
+// burger.addEventListener('mouseover', show);
+
+// ul.addEventListener('click', () => {
+//     burger.classList.remove('active');
+//   });
 
 
