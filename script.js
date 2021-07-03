eee41f1b1f84662b7bc06534778fcab6d4e22c72
Claire <avenel.cl@gmail.com>
2021-07-03 17:45:35 +0200

    essais infructueux

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-function showResponsiveMenu() {
-    var menu = document.getElementById("topnav_menu");
-    var icon = document.getElementById("topnav_icon");
-    var root = document.getElementById("root");
 
-    if (menu.className === "") {
-      menu.className = "open";
-      icon.className = "open";
-    } else {
-      menu.className = "";                    
-      icon.className = "";
-    }
-    
-  }
   function showMenu() {
     var menu = document.getElementById("topnav_menu");
     var icon = document.getElementById("topnav_icon");
@@ -26,6 +13,7 @@ function showResponsiveMenu() {
     }
     
   }
+
  
 // Chaussures
     var chaussures = document.getElementById("chaussures");
@@ -69,4 +57,94 @@ burger.addEventListener ('click', ()=>{
 });
 
 
+//  MENU RESPONSIVE
+var menu = document.getElementById("topnav_menu");
+var icon = document.getElementById("topnav_icon");
 
+
+burger.addEventListener ('click', ()=>{
+  menu.classList.toggle('open');
+});
+
+
+
+
+// ! test menu claire
+//   var menu = document.getElementById("topnav_menu");
+//   var icon = document.getElementById("topnav_icon");
+
+//   icon.addEventListener ('mouseover', ()=>{
+//     menu.classList.toggle('open');
+// });
+// menu.addEventListener ('mouseover', ()=>{
+//   menu.classList.toggle('');
+// });
+
+
+// ! test API
+// const options = {
+//   root : null,
+//   rootMargin: '0px',
+//   threshold: 0.5
+// }
+// let observer;
+// let target = document.querySelector('.yeah');
+// // observer.observe(target);
+
+// let callback = (entries, observer) => {
+//   entries.forEach(entry => {
+    
+//     // Each entry describes an intersection change for one observed
+//     // target element:
+//       // entry.boundingClientRect
+//       // entry.intersectionRatio
+//       // entry.intersectionRect
+//       // entry.isIntersecting
+//       // entry.rootBounds
+//       entry.observe(target)
+//       entry.time
+//   });
+// };
+// observer = new IntersectionObserver(callback, options);
+
+
+
+
+
+
+
+
+
+// const handleIntersect = function (entries, observer) {
+//   entries.forEach(function (entry){
+//     if(entry.intersectionRatio> threshold) {
+//       entry.target.classList.remove('yeah')
+//       observer.unobserve(entry.target)
+//     }
+//   })
+// }
+// document.documentElement.classList.add('API')
+
+//   const observer = new IntersectionObserver(handleIntersect, options)
+//   const targets = document.querySelectorAll('.yeah')
+//   targets.forEach(function (target) {
+//     observer.observe(target)
+//   })
+
+
+
+
+
+
+
+
+
+// let observer = new IntersectionObserver ((entries) =>
+// {
+//   entries.forEach((entry) => {
+//     threshold: 0.5
+//   });
+// })
+
+// let API= document.querySelector('.API');
+// observer.observe(API, 'yeah');
