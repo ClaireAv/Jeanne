5633ef0b14ee2dbf0628d86b2dc562ceb695a700
Claire <avenel.cl@gmail.com>
2021-07-03 14:08:17 +0200

    menu responsive ok

--- script.js
+++ script.js
@@ -26,18 +26,47 @@ function showResponsiveMenu() {
     }
     
   }
+ 
+// Chaussures
+    var chaussures = document.getElementById("chaussures");
+    var listeChaussures = document.getElementById("listeChaussures");
 
+    chaussures.addEventListener ('click', ()=>{
+      listeChaussures.classList.toggle('open');
+  });
+//  SACS
+  var sacs = document.getElementById("sacs");
+  var listeSacs = document.getElementById("listeSacs");
+
+  sacs.addEventListener ('click', ()=>{
+    listeSacs.classList.toggle('open');
+});
+
+//  robes
+var robes = document.getElementById("robes");
+  var listeRobes = document.getElementById("listeRobes");
+
+    robes.addEventListener ('click', ()=>{
+    listeRobes.classList.toggle('open');
+});
+
+// BIJOUX
+
+var bijoux = document.getElementById("bijoux");
+  var listeBijoux = document.getElementById("listeBijoux");
+
+    bijoux.addEventListener ('click', ()=>{
+    listeBijoux.classList.toggle('open');
+});
+
+   
+
+// Burger  croix
 const burger = document.querySelector('.burger');
-// const ul = document.querySelector('ul');
 
 burger.addEventListener ('click', ()=>{
     burger.classList.toggle('active');
 });
 
-// burger.addEventListener('mouseover', show);
-
-// ul.addEventListener('click', () => {
-//     burger.classList.remove('active');
-//   });
 
 
